0717b8fc141ab29fa80a608b3922f239de681d2f
Samuel Atkins <s.m.atkins73@gmail.com>
2020-05-11 13:50:06 -0700

    11/05/2020-Atkins-13:50:06 Finished 387

---
+++ 387-first-unique-character-in-a-string.py
@@ -0,0 +1,48 @@
+# My solution:
+'''
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen_dict = {}
+        seen_index_dict = {}
+        for i in range(len(s)):
+            if seen_dict.get(s[i]) is not None:
+                if seen_dict[s[i]] == -1:
+                    continue
+                seen_index_dict.pop(seen_dict[s[i]])
+                seen_dict[s[i]] = -1
+
+            else:
+                seen_dict[s[i]] = i
+                seen_index_dict[i] = s[i]
+
+        for i in range(len(s)):
+            res = seen_index_dict.get(i)
+            if res is not None:
+                return seen_dict[res]
+        return -1
+'''
+
+# Elegant second try (slower somehow):
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen_dict = {}
+
+        for i in range(len(s)):
+            if seen_dict.get(s[i]):
+                seen_dict[s[i]] += 1
+            else:
+                seen_dict[s[i]] = 1
+
+        for i in range(len(s)):
+            if seen_dict.get(s[i]):
+                if seen_dict[s[i]] < 2:
+                    return i
+        return -1
