0d75d6b02f934daee34dbd3def335a6bda2e9c1e
Samuel Atkins <s.m.atkins73@gmail.com>
2020-06-09 14:32:39 -0700

    06-09-2020-Atkins Completed 69 with math library solution. Need to revisit with manual solution. 7th.

---
+++ 69-sqrtx.py
@@ -0,0 +1,12 @@
+# Simple solution using math library (cheating): 28ms, 42.9%
+#
+# import math
+# class Solution(object):
+#     def mySqrt(self, x):
+#         """
+#         :type x: int
+#         :rtype: int
+#         """
+#         return int(math.floor((math.sqrt(x))))
+
+# Solution without math library:
