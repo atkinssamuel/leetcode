ad8de2a41dcda24adfd3c40732a31814630032df
Samuel Atkins <s.m.atkins73@gmail.com>
2020-06-16 23:01:25 -0700

    06-16-2020-Atkins Completed 292. Second.

---
+++ 292-nim-game.py
@@ -0,0 +1,10 @@
+# First pass solution: 74.06%, 16ms
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n % 4 == 0:
+            return False
+        return True
